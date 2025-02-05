e9a19d7ddc9099a7b41a87f36909c28c08f2620a
Noelia98-cmd <lea.noelia98@gmail.com>
2025-02-05 13:03:17 +0100

    Validación inicial

---
+++ proyecto_git_NOELIA_LEA/src/git/ClaseRamaMaster.java
@@ -0,0 +1,10 @@
+package git;
+
+public class ClaseRamaMaster {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Primer fichero del paquete git en la rama Master");
+	}
+
+}
